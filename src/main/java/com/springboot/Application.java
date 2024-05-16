094d46197dc5e051959cbb37419e362aa19b219a
AfridiAslam <afridiertugrul96@gmail.com>
2024-05-16 21:25:44 +0530

    Variable Added

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -10,7 +10,7 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
 		
-		run.close();
+		int i=1000;
 	}
 
 }
